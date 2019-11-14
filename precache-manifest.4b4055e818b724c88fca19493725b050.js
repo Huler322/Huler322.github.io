810bd10e4c950d240216a7c4b658228a2093a9ed
Anatoliy <tarnovskidj@gmail.com>
2019-11-14 15:50:34 +0200

    bra

---
+++ precache-manifest.4b4055e818b724c88fca19493725b050.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ca87b396f2d154476c2b5383f309b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c155b4a43fca955333f",
+    "url": "/static/css/main.c61424e8.chunk.css"
+  },
+  {
+    "revision": "b99e53b1231358decf38",
+    "url": "/static/js/2.0480bfa7.chunk.js"
+  },
+  {
+    "revision": "8c155b4a43fca955333f",
+    "url": "/static/js/main.4d0de745.chunk.js"
+  },
+  {
+    "revision": "72021a95e336f4da9f10",
+    "url": "/static/js/runtime-main.11dda465.js"
+  },
+  {
+    "revision": "b6f28417fad2353c42a516737d13a1b0",
+    "url": "/static/media/book.b6f28417.png"
+  }
+]);
